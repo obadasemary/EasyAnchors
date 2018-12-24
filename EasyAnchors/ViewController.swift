d2e57e9c180e7f1cd6d759b46a5a385cfceff13f
Abdelrahman Mohamed <obada.semary@gmail.com>
2018-12-24 17:02:08 +0300

    Add extension UIView Add anchor func

--- EasyAnchors/ViewController.swift
+++ EasyAnchors/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        view.addSubview(redView)
+        
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
+}
 
-
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, trailing: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        topAnchor.constraint(equalTo: top).isActive = true
+        leadingAnchor.constraint(equalTo: leading).isActive = true
+        trailingAnchor.constraint(equalTo: trailing).isActive = true
+        bottomAnchor.constraint(equalTo: bottom).isActive = true
+    }
 }
 
