5a4ab5c4ae2ccc333070b851368023059b2bd4da
Abdelrahman Mohamed <obada.semary@gmail.com>
2019-01-08 19:51:24 +0300

    Fix Some issues Add some general func

--- EasyAnchors/ViewController.swift
+++ EasyAnchors/ViewController.swift
@@ -16,21 +16,74 @@ class ViewController: UIViewController {
         let redView = UIView()
         redView.backgroundColor = .red
         
-        view.addSubview(redView)
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
         
-        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor)
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        
+        let purpleView = UIView()
+        purpleView.backgroundColor = .purple
+        view.addSubview(purpleView)
+        purpleView.fillSuperview()
+        
+        [redView, blueView, greenView].forEach { (v) in
+            view.addSubview(v)
+        }
+        
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: UIEdgeInsets.init(top: 12, left: 0, bottom: 0, right: 12), size: CGSize.init(width: 125, height: 0))
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        
+        blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: UIEdgeInsets.init(top: 12, left: 0, bottom: 0, right: 0))
+        
+        blueView.anchorSize(to: redView)
+        
+//        blueView.widthAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+//        blueView.heightAnchor.constraint(equalTo: blueView.widthAnchor).isActive = true
+        
+        greenView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: UIEdgeInsets.init(top: 12, left: 12, bottom: 0, right: 12))
     }
 }
 
 extension UIView {
     
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, trailing: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor) {
+    func fillSuperview() {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    }
+    
+    func anchorSize(to view: UIView) {
+        
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        
         translatesAutoresizingMaskIntoConstraints = false
         
-        topAnchor.constraint(equalTo: top).isActive = true
-        leadingAnchor.constraint(equalTo: leading).isActive = true
-        trailingAnchor.constraint(equalTo: trailing).isActive = true
-        bottomAnchor.constraint(equalTo: bottom).isActive = true
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
     }
 }
 
